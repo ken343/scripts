3b888ba0b203557cbc824776e3a5ef9c8a9a3e41
ken343 <kmcdole21@gmail.com>
2020-03-02 00:55:41 -0600

    Adding Init.vim

--- my_script.sh
+++ my_script.sh
@@ -9,14 +9,60 @@ mkdir -m ~/.config/nvim/init.vim
 mkdir -p ~/.local/share/nvim/site/plugin
 mkdir -p ~/.local/share/nvim/site/ftplugin
 
+# Install VimPlug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # Configure Key Mappings
 cat "inoremap jj <Esc>" >> ~/.config/nvim/init.vim
 cat "set number" >> ~/.config/nvim/init.vim
+cat << EOF >> ~/config/nvim/init.vim
+" Specify a directory for plugins
+" - For Neovim: stdpath('data') . '/plugged'
+" - Avoid using standard Vim directory names like 'plugin'
+call plug#begin('~/.local/share/site/plugged)
+
+" Make sure you use single quotes
+
+" Shorthand notation; fetches https://github.com/junegunn/vim-easy-align
+Plug 'junegunn/vim-easy-align'
+
+" Any valid git URL is allowed
+Plug 'https://github.com/junegunn/vim-github-dashboard.git'
+
+" Multiple Plug commands can be written in a single line using | separators
+Plug 'SirVer/ultisnips' | Plug 'honza/vim-snippets'
+
+" On-demand loading
+Plug 'scrooloose/nerdtree', { 'on':  'NERDTreeToggle' }
+Plug 'tpope/vim-fireplace', { 'for': 'clojure' }
+
+" Using a non-master branch
+Plug 'rdnetto/YCM-Generator', { 'branch': 'stable' }
+
+" Using a tagged release; wildcard allowed (requires git 1.9.2 or above)
+Plug 'fatih/vim-go', { 'tag': '*' }
+
+" Plugin options
+Plug 'nsf/gocode', { 'tag': 'v.20150303', 'rtp': 'vim' }
+
+" Plugin outside ~/.vim/plugged with post-update hook
+Plug 'junegunn/fzf', { 'dir': '~/.fzf', 'do': './install --all' }
+
+" Unmanaged plugin (manually installed and updated)
+Plug '~/my-prototype-plugin'
+
+" Initialize plugin system
+call plug#end()
+EOF
 
 # Install Fugitive Plugin (Git Wrapper) by tpope
 git clone https://tpope.io/vim/fugitive.git ~/.local/share/nvim/site/plugin
 vim -u NONE -c "helptags fugitive/doc" -c q
 
+
+# Install NerdTree
+git clone https://github.com/preservim/nerdtree.git ~/.local/share/nvim/site/plugin
+vim -u NONE -c "helptags ~/.local/share/nvim/site/plugin/nerdtree/doc" -c q
 
 #=========================================================================
 
