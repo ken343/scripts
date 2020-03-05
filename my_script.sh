88f976f0b893fb8fbae7dd4240fd03e6db2b46ae
ken343 <kmcdole21@gmail.com>
2020-03-05 07:15:37 +0000

    made script not sudo. fixed typos

--- my_script.sh
+++ my_script.sh
@@ -2,67 +2,58 @@
 
 #=========================================================================
 
-# Install NeoVim and Configure it & Plugins
-apt install neovim
+# Install Vim and Configure it & Plugins
 
-mkdir -m ~/.config/nvim/init.vim
-mkdir -p ~/.local/share/nvim/site/plugin
-mkdir -p ~/.local/share/nvim/site/ftplugin
+mkdir -m ~/.vim/vimrc
+mkdir -p ~/.vim/plugged
 
 # Install VimPlug
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Configure Key Mappings
-cat "inoremap jj <Esc>" >> ~/.config/nvim/init.vim
-cat "set number" >> ~/.config/nvim/init.vim
-cat << EOF >> ~/config/nvim/init.vim
+cat "inoremap jj <Esc>" >> ~/.vim/vimrc
+cat "set number" >> ~/.vim/vimrc
+
+cat << EOF >> ~/.vim/vimrc
 " Specify a directory for plugins
 " - For Neovim: stdpath('data') . '/plugged'
 " - Avoid using standard Vim directory names like 'plugin'
-call plug#begin('~/.local/share/site/plugged)
+"call plug#begin('~/.vim/plugged')
 
 " Make sure you use single quotes
 
 " Shorthand notation; fetches https://github.com/junegunn/vim-easy-align
-Plug 'junegunn/vim-easy-align'
+"Plug 'junegunn/vim-easy-align'
 
 " Any valid git URL is allowed
-Plug 'https://github.com/junegunn/vim-github-dashboard.git'
+"Plug 'https://github.com/junegunn/vim-github-dashboard.git'
 
 " Multiple Plug commands can be written in a single line using | separators
-Plug 'SirVer/ultisnips' | Plug 'honza/vim-snippets'
+"Plug 'SirVer/ultisnips' | Plug 'honza/vim-snippets'
 
 " On-demand loading
-Plug 'scrooloose/nerdtree', { 'on':  'NERDTreeToggle' }
-Plug 'tpope/vim-fireplace', { 'for': 'clojure' }
+"Plug 'scrooloose/nerdtree', { 'on':  'NERDTreeToggle' }
+"Plug 'tpope/vim-fireplace', { 'for': 'clojure' }
 
 " Using a non-master branch
-Plug 'rdnetto/YCM-Generator', { 'branch': 'stable' }
+"Plug 'rdnetto/YCM-Generator', { 'branch': 'stable' }
 
 " Using a tagged release; wildcard allowed (requires git 1.9.2 or above)
-Plug 'fatih/vim-go', { 'tag': '*' }
+"Plug 'fatih/vim-go', { 'tag': '*' }
 
 " Plugin options
-Plug 'nsf/gocode', { 'tag': 'v.20150303', 'rtp': 'vim' }
+"Plug 'nsf/gocode', { 'tag': 'v.20150303', 'rtp': 'vim' }
 
 " Plugin outside ~/.vim/plugged with post-update hook
-Plug 'junegunn/fzf', { 'dir': '~/.fzf', 'do': './install --all' }
+"Plug 'junegunn/fzf', { 'dir': '~/.fzf', 'do': './install --all' }
 
 " Unmanaged plugin (manually installed and updated)
-Plug '~/my-prototype-plugin'
+"Plug '~/my-prototype-plugin'
 
 " Initialize plugin system
-call plug#end()
+"call plug#end()
 EOF
-
-# Install Fugitive Plugin (Git Wrapper) by tpope
-git clone https://tpope.io/vim/fugitive.git ~/.local/share/nvim/site/plugin
-vim -u NONE -c "helptags fugitive/doc" -c q
-
-
-# Install NerdTree
-git clone https://github.com/preservim/nerdtree.git ~/.local/share/nvim/site/plugin
-vim -u NONE -c "helptags ~/.local/share/nvim/site/plugin/nerdtree/doc" -c q
 
 #=========================================================================
 
@@ -76,15 +67,16 @@ snap install code \
 	go
 
 #############################################################################
-: ' This will set up kubernetes and docker
+# This will set up kubernetes and docker
 # s script will set up the node to be a master node.
 
 echo Setting up Kubernetes Node...
 
-# Update instance repos and install standard software.
-sudo apt-get update
+swapoff -a && sed -i / swap / s/^/#/ /etc/fstab
 
-sudo su -
+# Update instance repos and install standard software.
+apt-get update
+
 
 apt-get install \
     apt-transport-https \
@@ -94,7 +86,7 @@ apt-get install \
     software-properties-common
 
 # Add key for docker-ubuntu repo
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 # Add repo to end of /etc/apt/source.list
 add-apt-repository \
@@ -108,8 +100,8 @@ apt-get install docker-ce docker-ce-cli containerd.io
 
 # Update repo list with kubernetes tools and install the 3 universal tools
 # all nodes are expected to have.
-apt-get update && sudo apt-get install -y apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+apt-get update && apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
@@ -120,7 +112,6 @@ apt-get install -y kubelet kubeadm kubectl
 # The next portions are specific to the master node.
 
 # Kubeadm prepares master node
-sudu su -
 
 kubeadm init
 
@@ -129,8 +120,8 @@ exit
 # Make a directory with kubernetes configuration inside HOME.
 # Give file super user permissions?
 mkdir -p $HOME/.kube
-  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-  sudo chown $(id -u):$(id -g) $HOME/.kube/config
+  cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  chown $(id -u):$(id -g) $HOME/.kube/config
 
 # Check that what nodes are currently in cluster.
 # Then check what pods are available across all namespaces.
@@ -138,7 +129,6 @@ kubectl get node
 kubectl get pods --all-namespaces
 
 # I have no idea what is going on here.
-sudo su -
 sysctl net.bridge.bridge-nf-call-iptables=1
 
 # Start clouds.weave.works with kubernetes version passed in
@@ -150,4 +140,4 @@ exit
 
 # Check for all nodes once more.
 kubectl get nodes
-'
+
